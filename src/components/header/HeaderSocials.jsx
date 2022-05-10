ca69b4037ccc51cf2cb0358828038b0d11dc056f
Diana <dmmolano@yahoo.com>
2022-05-10 02:35:56 -0700

    set up links to socials

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://linkedin.com/in/diana-cady-8a8a0135" target="_blank"></a>
+        <a href="https://https://github.com/molano1979" target="_blank"></a>
+    
+    </div>
+  )
+}
+
+export default HeaderSocials
