3f494cf021266e1bb027bc086d10ffe76944e169
Diana <dmmolano@yahoo.com>
2022-05-10 04:47:07 -0700

    adjusted profile pic in header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpeg'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Diana Cady</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {GoMarkGithub} from 'react-icons/go'
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com/in/diana-cady-8a8a0135" target="_blank"></a>
-        <a href="https://https://github.com/molano1979" target="_blank"></a>
+        <a href="https://linkedin.com/in/diana-cady-8a8a0135" target="_blank"><BsLinkedin/></a>
+        <a href="https://https://github.com/molano1979" target="_blank"><GoMarkGithub/></a>
     
     </div>
   )
