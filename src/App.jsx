4ebbd89ba4d1ce228a2f357df89f4914d1d28134
Diana <dmmolano@yahoo.com>
2022-05-09 23:24:55 -0700

    picked temp colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 export const App = () => {
   return (
@@ -9,6 +15,13 @@ export const App = () => {
       <Header />
       <Nav />
       <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
     </>
   )
 }
