dd8a633e170510e31d29fec1c954e6a7bf84c9f1
Diana <dmmolano@yahoo.com>
2022-05-09 23:02:25 -0700

    added more components folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Nav />
+      <About />
     </>
   )
 }
