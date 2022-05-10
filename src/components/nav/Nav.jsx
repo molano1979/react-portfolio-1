d50b7398581a9ba5b02b8f42910a7b84d95e9019
Diana <dmmolano@yahoo.com>
2022-05-10 05:46:33 -0700

    styling nav section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {FiBriefcase} from 'react-icons/fi'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
 
 function Nav() {
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineHome/></a>
-      <a href="#experience"><AiOutlineHome/></a>
-      <a href="#portfolio"><AiOutlineHome/></a>
-      <a href="#contact"><AiOutlineHome/></a>
+      <a href="#"
+      className='active'><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><FiBriefcase/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
     </nav>
   )
 }
