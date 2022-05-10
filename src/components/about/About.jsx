6a2979e14d631b3873ddb33ce8c4a59b8963fe8d
Diana <dmmolano@yahoo.com>
2022-05-10 13:06:56 -0700

    fixed link to github

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/pic.png'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+
+          </div>
+        
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about_card'>
+              
+
+            </article>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
   return (
     <div className='header_socials'>
         <a href="https://linkedin.com/in/diana-cady-8a8a0135" target="_blank"><BsLinkedin/></a>
-        <a href="https://https://github.com/molano1979" target="_blank"><GoMarkGithub/></a>
+        <a href="https://github.com/molano1979" target="_blank"><GoMarkGithub/></a>
     
     </div>
   )
