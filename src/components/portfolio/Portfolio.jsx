7a1231cefce523ad3cfa3f961c8d588797233ead
Diana <dmmolano@yahoo.com>
2022-05-12 11:50:54 -0700

    sized portfolio images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,53 +4,61 @@ import IMG1 from '../../assets/cardiac.png'
 import IMG2 from '../../assets/workday.png'
 import IMG3 from '../../assets/note.png'
 
-function Portfolio() {
-  return (
-    <section id='portfolio'>
-    <h5>My Recent Work</h5>
-    <h2>Portfolio</h2>
 
-    <div className="container portfolio_container">
-     
-     {/*cardiac arrest*/}
-      <article className='portfolio_item'>
-        <div className="portfolio_item-image">
-          <img src={IMG1} alt="cardiac"/>
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Cardiac Arrest',
+    github: 'https://github.com/molano1979/Cardiac-Arrest',
+    demo: 'https://floating-temple-42927.herokuapp.com/login'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Worday Scheduler',
+    github: 'https://github.com/molano1979/Work-Day-Scheduler',
+    demo: 'https://molano1979.github.io/Work-Day-Scheduler/'
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Note Taker',
+    github: 'https://github.com/molano1979/note-taker',
+    demo: 'https://mysterious-spire-09295.herokuapp.com/notes'
+  },
+
+]
+
+
+
+  const Portfolio = () => {
+    return (
+      <section id='portfolio'>
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+  
+        <div className="container portfolio__container">
+          {
+            data.map(({id, image, title, github, demo}) => {
+              return (
+                <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+              )
+            })
+          }
         </div>
-        <h3>Cardiac Arrest</h3>
-        <a href="https://github.com/molano1979/Cardiac-Arrest" className='btn' target='_blank'>Github</a>
-        <a href="https://floating-temple-42927.herokuapp.com/login" className='btn btn-primary' target='_blank'>Live Demo</a>
-
-
-      </article>
-
- {/*wordayscheduler*/}
-<article className='portfolio_item'>
-        <div className="portfolio_item-image">
-          <img src={IMG2} alt="workday"/>
-        </div>
-        <h3>Work Day Scheduler</h3>
-        <a href="https://github.com/molano1979/Work-Day-Scheduler" className='btn' target='_blank'>Github</a>
-        <a href="https://molano1979.github.io/Work-Day-Scheduler/" className='btn btn-primary' target='_blank'>Live Demo</a>
-
-
-      </article>
-
- {/*note taker*/}
-  <article className='portfolio_item'>
-        <div className="portfolio_item-image">
-          <img src={IMG3} alt="notes"/>
-        </div>
-        <h3>Note Taker</h3>
-        <a href="https://github.com/molano1979/note-taker" className='btn' target='_blank'>Github</a>
-        <a href="https://mysterious-spire-09295.herokuapp.com/notes" className='btn btn-primary' target='_blank'>Live Demo</a>
-
-
-      </article>
-
-    </div>
-    </section>
-  )
-}
-
-export default Portfolio
+      </section>
+    )
+  }
+  
+  export default Portfolio
