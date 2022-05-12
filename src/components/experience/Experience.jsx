94d9a289bd99cf80ab156cd39b5920734a40197d
Diana <dmmolano@yahoo.com>
2022-05-12 07:07:33 -0700

    adding content to the experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Skills</h5>
+      <h2>My Experience</h2>
+      
+      
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience-details'></article>
+            <BsPatchCheckFill />
+
+
+          </div>
+
+        </div>
+        <div className="experience_backend">
+
+        </div>
+      
+      </div>
+      </section>
+ 
+
   )
 }
 
