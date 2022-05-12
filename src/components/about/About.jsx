570a40571532ecdbe2a284c45c60d51827c9f2b1
Diana <dmmolano@yahoo.com>
2022-05-12 05:49:51 -0700

    added lets talk button

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me.png'
-import {FaAward} from 'react-icons/fa'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -19,19 +21,31 @@ const About = () => {
         <div className="about_content">
           <div className="about_cards">
             <article className='about_card'>
-              <FaAward className='about_icon'/>
+              <FaAward className='about_icon' />
               <h5>Experience</h5>
               <small></small>
 
             </article>
             <article className='about_card'>
-              <FaAward className='about_icon'/>
-              <h5>Experience</h5>
-              <small>6 months</small>
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small></small>
 
             </article>
-          </div>
 
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+
+            </article>
+
+          </div>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consectetur id ipsa iusto minima modi laboriosam maiores dolores facilis quisquam, quia soluta aliquam, aliquid ratione! Sed ea nisi eum. Odio!
+</p>
+
+<a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
 
       </div>
