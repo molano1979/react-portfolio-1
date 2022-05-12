f2a3212d4c4a9194b715791627a996773510f34a
Diana <dmmolano@yahoo.com>
2022-05-12 10:46:48 -0700

    mapped first portfolio item cardiac arrest

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/cardiac.png'
+import IMG2 from '../../assets/workday.png'
+import IMG3 from '../../assets/note.png'
 
 function Portfolio() {
   return (
@@ -9,9 +12,13 @@ function Portfolio() {
 
     <div className="container portfolio_container">
       <article className='portfolio_item'>
-        <div className="portfolio_item-image"></div>
+        <div className="portfolio_item-image">
+          <img src={IMG1} alt="cardiac-arrest"/>
+        </div>
         <h3>Cardiac Arrest</h3>
         <a href="https://github.com/molano1979/Cardiac-Arrest" className='btn'>Github</a>
+        <a href="https://floating-temple-42927.herokuapp.com/login" className='btn btn-primary' target='_blank'>Live Demo</a>
+
 
       </article>
 
