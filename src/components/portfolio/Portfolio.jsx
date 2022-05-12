208e332c243aed16506c90e4c27e3d17c328eba9
Diana <dmmolano@yahoo.com>
2022-05-12 10:33:30 -0700

    adding links to the portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,6 +5,17 @@ function Portfolio() {
   return (
     <section id='portfolio'>Portfolio>
     <h5>My Recent Work</h5>
+    <h2>Portfolio</h2>
+
+    <div className="container portfolio_container">
+      <article className='portfolio_item'>
+        <div className="portfolio_item-image"></div>
+        <h3>Cardiac Arrest</h3>
+        <a href="https://github.com/molano1979/Cardiac-Arrest" className='btn'>Github</a>
+
+      </article>
+
+    </div>
     </section>
   )
 }
