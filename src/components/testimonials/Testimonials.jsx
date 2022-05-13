512a531802b3a4103e8728e3be72ecd88ba333ec
Diana <dmmolano@yahoo.com>
2022-05-12 17:33:59 -0700

    adding first avatar

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
 <h5>Reviews from clients</h5>
 <h2>Testimonials</h2>
+<div className="container testimonials_container">
+  <article className="testimonial">
+    <div className="client_avitar">
+      <img src={AVTR1} alt="Avatar One" />
+    </div>
+  </article>
+
+</div>
 
     </section>
     
